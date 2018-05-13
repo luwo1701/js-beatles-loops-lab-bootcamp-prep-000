9ee31fd5160dce506fc8126950c8f2e8ac853596
Luke Worley <luke.worley@colorado.edu>
2018-05-13 19:26:31 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function theBeatlesPlay(musicians,instruments){
+  var arr=[]
+  var i;
+  for (i = 0; i < musicians.length; i++) { 
+    arr.push(musicians[i] + ' plays '+ instruments[i])
+  }
+  return arr
+}
+function johnLennonFacts(facts){
+  var arr=[]
+  while(facts.length>0){
+    arr.push(facts[0] + "!!!")
+    facts.shift()
+  }
+  return arr
+}
+
+function iLoveTheBeatles(n){
+  var arr=[]
+  
+  do{
+    arr.push("I love the Beatles!")
+  }while(n<15)
+  return arr
+  
+}
